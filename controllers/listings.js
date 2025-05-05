f60ecc55eb5ce36e7505512a4c35dd3a8028cff4
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-05-05 20:12:20 +0530

    Refine project implementation

--- controllers/listings.js
+++ controllers/listings.js
@@ -11,9 +11,7 @@
 
 
 
-// module.exports.renderNewForm = (req, res)=> {
-//     res.render("listings/new.ejs" , {listing: {} });
-// }
+
 
 // module.exports.showListing = async(req,res) => {
 //     const{id} = req.params;
