0c6bac26560201f11845e52798f2bae0167c307f
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-05-09 20:12:20 +0530

    Update project features

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,10 +1,4 @@
-﻿        //     let url = req.file.path;
-//     let filename = req.file.filename;       
-//     const newListing = new Listing(req.body.listing);
-//     newListing.owner = req.user._id;
-//     newListing.image = {url, filename};
-
-//     newListing.geometry = response.body.features[0].geometry;
+﻿//     newListing.geometry = response.body.features[0].geometry;
 
 //     let savedListing = await newListing.save();
 //     console.log(savedListing);
