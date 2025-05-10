94d81a77e931c2d28bd57f218d83d5f4c4980c39
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-05-10 20:12:20 +0530

    Implement project improvements

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,4 @@
-﻿// module.exports.updateListing = async(req,res) => {       
-//         let{ id } = req.params;
-//         let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing}); 
-
+﻿
 //         if (typeof req.file !=="undefined") {
 //         let url = req.file.path;
 //         let filename = req.file.filename;
