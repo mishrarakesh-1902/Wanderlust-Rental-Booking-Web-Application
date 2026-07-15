3540b3afc365b415eac5909a587ba87759fd4324
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2026-07-15 20:12:20 +0530

    chnage listing.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,9 +1,4 @@
-﻿// const Listing = require("../models/listing");
-// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-// const mapToken = process.env.MAP_TOKEN;
-// const geocodingClient = mbxGeocoding({ accessToken: mapToken });
-
-
+﻿
 
 
 // // module.exports.index = async (req, res) => {
